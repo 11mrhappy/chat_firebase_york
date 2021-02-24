88e50b4d3c601e44dd1fc1c5cf3c02249061bfd1
11mrhappy <nekomamiremy@gmail.com>
2021-02-24 17:50:33 +0900

    done

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:chat_firebase_york/pages/top_page.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
